e14a1dd347860cec0aa2195fa43decc4f0d9c61f
adr <adrian.lenard@me.com>
2022-01-24 14:35:38 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,63 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
+require('@nomiclabs/hardhat-etherscan');
+require("hardhat-gas-reporter");
 require("@atixlabs/hardhat-time-n-mine");
+//require("hardhat-interface-generator");
+require('dotenv').config();
+const balance = require("@openzeppelin/test-helpers/src/balance");
+const { abi2sol } = require("./scripts/abi2sol.js");
 
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+task("abi2interface", "Generates solidity interface contracts from ABIs")
+  .addPositionalParam("contract", "Solidity contract name")
+  .setAction(async ({ contract }) => {
+    await abi2sol(contract);
+  });
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+gasPriceApis = {
+  ropsten: 'https://api-ropsten.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=' + process.env.ETHERSCAN_API_KEY,
+  rinkeby: 'https://api-rinkeby.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=' + process.env.ETHERSCAN_API_KEY,
+  mainnet: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=' + process.env.ETHERSCAN_API_KEY,
+  hardhat: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=' + process.env.ETHERSCAN_API_KEY
+}
+
+let gasPriceApi;
+if (process.argv.includes('--network')) {
+  idx = process.argv.indexOf('--network');
+  gasPriceApi = gasPriceApis[process.argv[idx + 1]];
+}
+
+//[owner, proxyAdmin, alice, backend] = await ethers.getSigners();
+const envkeys = [
+  //process.env.PROXYADMIN_PRIVATE_KEY,
+  process.env.MULTISIG_PRIVATE_KEY,
+  process.env.ALICE_PRIVATE_KEY,
+  process.env.BACKEND_PRIVATE_KEY
+]
+
+const accountkeys = []
+for (const key of envkeys) {
+  if (typeof key !== 'undefined' && key !== '') {
+    accountkeys.push(key);
+  }
+}
 
 module.exports = {
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    //outputFile: './output.txt',
+    gasPriceApi: gasPriceApi,
+    noColors: true,
+    showTimeSpent: true,
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
@@ -30,15 +67,45 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       chainId: 31337
     },
+    mainnet: {
+      url: process.env.MAINNET_URL || '',
+      accounts: accountkeys,
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_TESNET_URL || '',
+      accounts: accountkeys,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_TESNET_URL || '',
+      accounts: accountkeys,
+      timeout: 5 * 60 * 1e3
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   solidity: {
-    version: "0.8.2",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+    compilers: [
+      { // Proxy contracts
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      { // Rand contracts
+        version: "0.8.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+    ],
+
   },
   paths: {
     sources: "./contracts",
@@ -47,6 +114,6 @@ module.exports = {
     artifacts: "./artifacts"
   },
   mocha: {
-    timeout: 20000
+    timeout: 5 * 60 * 1e3
   }
 }
