789124e98ada21e4dc155c97ceffd329c39b0b81
adr <adrian.lenard@me.com>
2022-03-08 13:34:34 +0100

    Address registry support

--- contracts/RandToken.sol
+++ contracts/RandToken.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./IAddressRegistry.sol"
 
 /// @title Rand.network ERC20 Token contract
 /// @author @adradr - Adrian Lenard
@@ -20,13 +21,12 @@ contract RandToken is
     PausableUpgradeable,
     AccessControlUpgradeable
 {
-    event SMAddressAndRoleUpdated(address newAddress);
+    event RegistryAddressUpdated(address newAddress);
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant SM_ROLE = keccak256("SM_ROLE");
 
-    address public SM_TOKEN;
+    IAddressRegistry public REGISTRY;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
@@ -36,11 +36,14 @@ contract RandToken is
     /// @param _name Name of the token like `Rand Token ERC20`
     /// @param _symbol Short symbol like `RND`
     /// @param _initialSupply Total supply to mint initially like `200e6`
+    /// @param _multisigVault is the address of multisig wallett, supply will be minted here
+    /// @param _registry is the address of address registry
     function initialize(
         string memory _name,
         string memory _symbol,
         uint256 _initialSupply,
-        address _multisigVault
+        address _multisigVault,
+        address _registry
     ) public initializer {
         __ERC20_init(_name, _symbol);
         __ERC20Burnable_init();
@@ -50,36 +53,44 @@ contract RandToken is
         _grantRole(DEFAULT_ADMIN_ROLE, _multisigVault);
         _grantRole(PAUSER_ROLE, _multisigVault);
         _grantRole(MINTER_ROLE, _multisigVault);
-        //_grantRole(SM_ROLE, _multisigVault);
         _mint(_multisigVault, _initialSupply * 10**decimals());
+
+        REGISTRY = _registry;
     }
 
-    /// @notice Function to allow SM to transfer funds when users would like to stake
-    /// @dev only accessible by the Safety Module contract via SM_ROLE
-    /// @param amount the amount of tokens to increase allowance for SM as spender on tokens of VC
-    function setAllowanceForSM(address staker, uint256 amount)
-        external
-        onlyRole(SM_ROLE)
-    {
-        require(SM_TOKEN != address(0x0), "RND: No SM address has been set");
-        uint256 currentAllowance = allowance(staker, address(SM_TOKEN));
-        amount += amount + currentAllowance;
-        _approve(staker, address(SM_TOKEN), amount);
+    // /// @notice Function to allow SM to transfer funds when users would like to stake
+    // /// @dev only accessible by the Safety Module contract via SM_ROLE
+    // /// @param amount the amount of tokens to increase allowance for SM as spender on tokens of VC
+    // function setAllowanceForSM(address staker, uint256 amount)
+    //     external
+    //     onlyRole(SM_ROLE)
+    // {
+    //     require(SM_TOKEN != address(0x0), "RND: No SM address has been set");
+    //     //uint256 currentAllowance = allowance(staker, address(SM_TOKEN));
+    //     //amount += amount + currentAllowance;
+    //     _approve(staker, address(SM_TOKEN), amount);
+    // }
+
+    function approveAndTransfer(
+        address owner,
+        address recipient,
+        uint256 amount
+    ) external {
+        require(REGISTRY.getAddress("SM") == _msgSender(), "RND: Not accessible by caller");
+        uint256 currentAllowance = allowance(owner, recipient);
+        _approve(owner, recipient, currentAllowance + amount);
+        _transfer(owner, recipient, amount);
     }
 
     /// @notice Function to let Rand to update the address of the Safety Module
-    /// @dev emits SMAddressUpdated() and only accessible by MultiSig
+    /// @dev emits RegistryAddressUpdated() and only accessible by MultiSig
     /// @param newAddress where the new Safety Module contract is located
-    function updateSMAddress(address newAddress)
+    function updateRegistryAddress(address newAddress)
         public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        SM_TOKEN = newAddress;
-        if (SM_TOKEN != address(0x0)) {
-            _revokeRole(SM_ROLE, SM_TOKEN);
-        }
-        _grantRole(SM_ROLE, newAddress);
-        emit SMAddressAndRoleUpdated(newAddress);
+        REGISTRY = newAddress;
+        emit RegistryAddressUpdated(newAddress);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -92,6 +103,13 @@ contract RandToken is
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    function burn(address account, uint256 amount)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _burn(account, amount);
     }
 
     /// @inheritdoc	ERC20Upgradeable
