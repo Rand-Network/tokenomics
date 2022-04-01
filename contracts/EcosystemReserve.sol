bdb08f084317adc0333d3cfd6ace0484eba75744
adr <adrian.lenard@me.com>
2022-04-01 18:36:14 +0200

    EcosystemReserve implementation

---
+++ contracts/EcosystemReserve.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./IAddressRegistry.sol";
+
+/// @title Rand.network Ecosystem Reserve
+/// @author @adradr - Adrian Lenard
+/// @notice Rand Ecosystem Reserve where Safety Module rewards are stored and distributed from
+contract EcosystemReserve is
+    Initializable,
+    UUPSUpgradeable,
+    PausableUpgradeable,
+    AccessControlUpgradeable
+{
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    IAddressRegistry public REGISTRY;
+
+    IERC20Upgradeable public REWARD_TOKEN;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    /// @notice Initializer allow proxy scheme
+    /// @dev For upgradability its necessary to use initialize instead of simple constructor
+    function initialize(IAddressRegistry _registry) public initializer {
+        __Pausable_init();
+        __AccessControl_init();
+
+        REGISTRY = _registry;
+        address _multisigVault = REGISTRY.getAddress("MS");
+        _grantRole(DEFAULT_ADMIN_ROLE, _multisigVault);
+        _grantRole(PAUSER_ROLE, _multisigVault);
+    }
+
+    function approve(
+        IERC20Upgradeable token,
+        address recipient,
+        uint256 amount
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) whenNotPaused {
+        token.approve(recipient, amount);
+    }
+
+    function transfer(
+        IERC20Upgradeable token,
+        address recipient,
+        uint256 amount
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) whenNotPaused {
+        token.transfer(recipient, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {}
+}
