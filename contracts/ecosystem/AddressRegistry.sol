0bd9fb296ccde5cb4f5ae7b42f7f38998b140884
adr <adrian.lenard@me.com>
2022-04-12 14:45:35 +0200

    ARY-01M: Inexistent Utilization of UUPS Dependency

--- contracts/ecosystem/AddressRegistry.sol
+++ contracts/ecosystem/AddressRegistry.sol
@@ -31,6 +31,7 @@ contract AddressRegistry is
         require(_multisigVault != address(0));
         __Pausable_init();
         __AccessControl_init();
+        __UUPSUpgradeable_init();
         _grantRole(DEFAULT_ADMIN_ROLE, _multisigVault);
         _grantRole(PAUSER_ROLE, _multisigVault);
     }
