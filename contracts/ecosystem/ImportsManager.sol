a561a2f4c7de0b12a8cae33156b4275c809063ef
adradr <adrian.lenard@me.com>
2023-10-17 21:54:10 +0200

    🔄 chore(ImportsManager.sol): update Solidity version from 0.8.2 to 0.8.4 🔥 chore(ImportsManager.sol): remove unused registry constants The Solidity version has been updated from 0.8.2 to 0.8.4 to take advantage of the latest features and improvements. Additionally, the unused registry constants have been removed to declutter the codebase.

--- contracts/ecosystem/ImportsManager.sol
+++ contracts/ecosystem/ImportsManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.2;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
@@ -28,16 +28,6 @@ contract ImportsManager is
     // Registry update related variables
     event RegistryAddressUpdated(IAddressRegistry newAddress);
     IAddressRegistry public REGISTRY;
-    // Registry constants
-    string public constant MULTISIG = "MS";
-    string public constant RAND_TOKEN = "RND";
-    string public constant VESTING_CONTROLLER = "VC";
-    string public constant VESTING_CONTROLLER_SIGNER = "VCS";
-    string public constant SAFETY_MODULE = "SM";
-    string public constant ECOSYSTEM_RESERVE = "RES";
-    string public constant GOVERNANCE = "GOV";
-    string public constant INVESTOR_NFT = "NFT";
-    string public constant POOL_TOKEN = "PT";
 
     function __ImportsManager_init() internal onlyInitializing {
         __Pausable_init();
