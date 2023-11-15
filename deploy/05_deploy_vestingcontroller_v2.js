d09a806f7205bc000de80809b5f149a9ed515a82
adradr <adrian.lenard@me.com>
2023-11-15 17:44:31 +0100

    Add SigTest_V2.sol contract for signature verification testing.
    Add deployment script for SigTest_V2 contract.
    
    Add deployment script for upgrading VestingController contract to V2.
    
    Add deployment script for upgrading VestingController contract.
    
    Remove unused functions from test_VestingController.js.
    
    Add new functions for deploying, minting and distributing tokens in test_VestingController.js.
    
    Initialize variables and contracts in test_VestingController.js.
    
    Finish it-statements in test_VestingController.js.

---
+++ deploy/05_deploy_vestingcontroller_v2.js
@@ -0,0 +1,53 @@
+require('hardhat-deploy');
+const { getUUPSImplementationAddress } = require("../test/utils");
+
+module.exports = async function (hre) {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy, execute } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    // Run fixture to deploy the contracts
+    //await deployments.fixture(['VestingControllerERC721'], { keepExistingDeployments: true });
+
+    // Get VC contract
+    const VestingControllerDeployment = await deployments.get("VestingControllerERC721");
+    const VestingControllerContract = await ethers.getContractFactory("VestingControllerERC721");
+    VestingController = await VestingControllerContract.attach(VestingControllerDeployment.address);
+
+    // Deploying V2
+    const vc_v2 = await deploy('VestingControllerERC721_V2', {
+        from: deployer,
+        proxy: false,
+        log: true,
+    });
+
+    // Log current proxy and implementation and V2 addresses
+    deployments.log("Proxy address:                     ", VestingControllerDeployment.address);
+    deployments.log("V1 implementation address:         ", VestingControllerDeployment.implementation);
+    deployments.log("V2 implementation address:         ", vc_v2.address);
+
+    // Upgrade the existing proxy to the new implementation
+    if (hre.network.config.chainId == "31337") {
+        // Upgrade proxy
+        tx = await VestingController.upgradeTo(vc_v2.address, { from: deployer });
+
+        // Mine a single block
+        await hre.network.provider.send("evm_mine");
+
+        // Get the new proxy implementation address
+        const vc_upgraded = await getUUPSImplementationAddress(VestingControllerDeployment);
+
+        // Log new proxy and implementation addresses
+        deployments.log("Proxy V2 implementation address:   ", vc_upgraded);
+    }
+
+    // Verifying contracts if not on local network
+    if (hre.network.config.chainId != "31337") {
+        await hre.run("etherscan-verify", { address: vc_v2.address }); // EtherScan verification
+        await hre.run("verify-proxy", { proxy: vc_v2.address, implementation: vc.implementation }); // OpenZeppelin proxy verification
+        await hre.run("sourcify", { address: vc_v2.address });   // Sourcify verification
+    };
+
+};
+
+module.exports.tags = ["VestingControllerERC721_V2"];
--- test/test_sigtest.js
+++ test/test_sigtest.js
@@ -7,7 +7,7 @@ async function getBlockTimestamp() {
     return Math.max(bts, ts) + 1800;
 }
 
-async function createSignature(signer_wallet, sender_addr, recipient_addr, amount, timestamp) {
+async function createSignature_v1(signer_wallet, sender_addr, recipient_addr, amount, timestamp) {
     // Wait 100ms to make sure the timestamp is different
     await new Promise(r => setTimeout(r, 1000));
     // Get the chainId
@@ -27,6 +27,29 @@ async function createSignature(signer_wallet, sender_addr, recipient_addr, amoun
     // STEP 3: To sign the 32 bytes of data, make sure you pass in the data
     return await signer_wallet.signMessage(messageHashBinary);
 }
+
+async function createSignature_v2(signer_wallet, sender_addr, recipient_addr, rndAmount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp) {
+    // Wait 100ms to make sure the timestamp is different
+    await new Promise(r => setTimeout(r, 1000));
+    // Get the chainId
+    chainId = (await ethers.provider.getNetwork()).chainId;
+
+    // STEP 1:
+    // building hash has to come from system address
+    // 32 bytes of data
+    let messageHash = ethers.solidityPackedKeccak256(
+        ["address", "address", "uint256", "uint256", "uint256", "uint256", "uint8", "uint256", "uint256"],
+        [sender_addr, recipient_addr, rndAmount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, chainId]
+    );
+
+    // STEP 2: 32 bytes of data in Uint8Array
+    let messageHashBinary = ethers.getBytes(messageHash);
+
+    // STEP 3: To sign the 32 bytes of data, make sure you pass in the data
+    return await signer_wallet.signMessage(messageHashBinary);
+}
+
+
 
 describe("SigTest", function () {
 
@@ -56,7 +79,7 @@ describe("SigTest", function () {
         recipient = userWallet.address;
         sender = userWallet;
 
-        signature = createSignature(adminWallet, userWallet.address, recipient, amount, timestamp);
+        signature = createSignature_v1(adminWallet, userWallet.address, recipient, amount, timestamp);
 
         // STEP 4: Fire off the transaction with the adminWallet signed data
         tx = await sigTest.connect(userWallet).redeemSignatureTest(recipient, amount, timestamp, signature)
@@ -71,26 +94,99 @@ describe("SigTest", function () {
     it("Try to verify with wrong amount", async function () {
         timestamp = await getBlockTimestamp();
         fail_amount = 300;
-        signature = createSignature(adminWallet, userWallet.address, recipient, amount, timestamp);
+        signature = createSignature_v1(adminWallet, userWallet.address, recipient, amount, timestamp);
         await expect(sigTest.connect(userWallet).redeemSignatureTest(recipient, fail_amount, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
     });
 
     it("Try to verify from wrong sender", async function () {
         timestamp = await getBlockTimestamp();
-        signature = createSignature(adminWallet, userWallet.address, recipient, amount, timestamp);
+        signature = createSignature_v1(adminWallet, userWallet.address, recipient, amount, timestamp);
         await expect(sigTest.connect(adminWallet).redeemSignatureTest(recipient, amount, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
     });
 
     it("Try to verify from wrong recipient", async function () {
         timestamp = await getBlockTimestamp();
-        signature = createSignature(adminWallet, userWallet.address, recipient, amount, timestamp);
+        signature = createSignature_v1(adminWallet, userWallet.address, recipient, amount, timestamp);
         await expect(sigTest.connect(userWallet).redeemSignatureTest(adminWallet.address, amount, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
     });
 
     it("Try to verify old timestamp", async function () {
         timestamp = await getBlockTimestamp() - 3600;
-        signature = createSignature(adminWallet, userWallet.address, recipient, amount, timestamp);
+        signature = createSignature_v1(adminWallet, userWallet.address, recipient, amount, timestamp);
         await expect(sigTest.connect(userWallet).redeemSignatureTest(recipient, amount, timestamp, signature)).to.be.revertedWith("VC: Signature has expired");
+    });
+
+
+});
+
+
+
+describe("SigTestV2", function () {
+
+    before(async function () {
+        // Fetching named accounts
+        const namedAccounts = await getNamedAccounts();
+        deployer = namedAccounts.deployer;
+        alice = namedAccounts.alice;
+
+        // Reset hardhat network
+        await hre.network.provider.send("hardhat_reset", []);
+
+        // Deploying contracts
+        await deployments.fixture(["SigTest_V2"]);
+
+        const SigTestDeployment = await deployments.get("SigTest_V2");
+        const SigTestContract = await ethers.getContractFactory("SigTest_V2");
+        sigTest = SigTestContract.attach(SigTestDeployment.address);
+    });
+
+    it("Verify", async function () {
+
+        [adminWallet, userWallet] = await ethers.getSigners();
+        // Create solidity block.timestamp compatible timestamp
+        timestamp = await getBlockTimestamp();
+        amount = 100;
+        vestingStartTime = timestamp;
+        vestingPeriod = 100;
+        cliffPeriod = 0;
+        nftLevel = 0;
+        recipient = userWallet.address;
+        sender = userWallet;
+
+        signature = createSignature_v2(adminWallet, userWallet.address, recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp);
+        // STEP 4: Fire off the transaction with the adminWallet signed data
+        tx = await sigTest.connect(userWallet).redeemSignatureTest(recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)
+        // Expect the transaction to succeed and return true
+        expect(await tx.wait()).to.emit(sigTest, "SignatureUsed").withArgs(userWallet.address, recipient, amount, timestamp, signature);
+    });
+
+    it("Try to verify same signature", async function () {
+        await expect(sigTest.connect(userWallet).redeemSignatureTest(recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)).to.be.revertedWith("VC: Signature already used");
+    });
+
+    it("Try to verify with wrong amount", async function () {
+        timestamp = await getBlockTimestamp();
+        fail_amount = 300;
+        signature = createSignature_v2(adminWallet, userWallet.address, recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp);
+        await expect(sigTest.connect(userWallet).redeemSignatureTest(recipient, fail_amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
+    });
+
+    it("Try to verify from wrong sender", async function () {
+        timestamp = await getBlockTimestamp();
+        signature = createSignature_v2(adminWallet, userWallet.address, recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp);
+        await expect(sigTest.connect(adminWallet).redeemSignatureTest(recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
+    });
+
+    it("Try to verify from wrong recipient", async function () {
+        timestamp = await getBlockTimestamp();
+        signature = createSignature_v2(adminWallet, userWallet.address, recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp);
+        await expect(sigTest.connect(userWallet).redeemSignatureTest(adminWallet.address, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)).to.be.revertedWith("VC: Signature not valid");
+    });
+
+    it("Try to verify old timestamp", async function () {
+        timestamp = await getBlockTimestamp() - 3600;
+        signature = createSignature_v2(adminWallet, userWallet.address, recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp);
+        await expect(sigTest.connect(userWallet).redeemSignatureTest(recipient, amount, vestingStartTime, vestingPeriod, cliffPeriod, nftLevel, timestamp, signature)).to.be.revertedWith("VC: Signature has expired");
     });
 
 
